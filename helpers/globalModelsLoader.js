e3b115d5ec379136d1c3c5d3581a2e0ce61418c6
Kunal Jain <kunal.jain@peoplegrove.com>
2025-10-16 04:07:35 +0530

    feat: added globalModelsLoader

---
+++ helpers/globalModelsLoader.js
@@ -0,0 +1,16 @@
+const models = require("../models");
+
+const globalModelsLoader = () => {
+  try {
+    Object.keys(models).forEach(modelName => {
+      const globalName = modelName.charAt(0).toUpperCase() + modelName.slice(1);
+      global[globalName] = models[modelName];
+    });
+    console.log('Models registered globally');
+  } catch (error) {
+    console.error('Error registering models globally - ', error);
+    throw error;
+  }
+}
+
+module.exports = globalModelsLoader;
