24f800d560bea4f0cbd682396062e71baddc9609
Kunal Jain <kunal.jain@peoplegrove.com>
2025-10-16 04:14:08 +0530

    feat: added job payload validator

---
+++ workers/mainWorker/validators/validateJobPayload.js
@@ -0,0 +1,32 @@
+const Joi = require("joi");
+const validateEntity = require("../../../utils/validateEntity");
+
+const jobPayloadSchema = Joi.object().keys({
+  workflowExecutionId: Joi.number().required(),
+  userWorkflowId: Joi.number().required(),
+  startNodeId: Joi.number().allow(null),
+  globalContext: Joi.object({
+    nodes: Joi.object(),
+    workflow: Joi.object(),
+  }).when('startNodeId', {
+    is: Joi.exist().not(null),
+    then: Joi.required(),
+    otherwise: Joi.optional(),
+  }),
+  isResume: Joi.boolean(),
+});
+
+const validateJobPayload = payload => {
+  try {
+    const isValid = validateEntity({
+      schema: jobPayloadSchema,
+      entity: payload,
+    });
+    return isValid;
+  } catch (error) {
+    console.error("Error in validators.validateJobPayload - ", error);
+    throw error;
+  }
+}
+
+module.exports = validateJobPayload;
