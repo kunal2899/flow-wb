710e1fbc298c138dccea488113d20b1990736a9b
Kunal Jain <kunal.jain@peoplegrove.com>
2025-10-16 04:14:50 +0530

    feat: added workers init entry file

---
+++ workers/index.js
@@ -0,0 +1,17 @@
+const mainWorker = require("./mainWorker");
+const globalModelsLoader = require("../helpers/globalModelsLoader");
+const { connectToServices } = require("../services/coreServices/dbConnectionService");
+
+const initialiseWorkers = async () => {
+  try {
+    await connectToServices();
+    globalModelsLoader();
+    await mainWorker.init();
+  } catch (error) {
+    console.error("Error in initializing workers - ", error);
+  }
+};
+
+(async () => {
+  await initialiseWorkers();
+})();
