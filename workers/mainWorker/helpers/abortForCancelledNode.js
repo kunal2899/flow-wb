16da45d20a802fa0fe46f7c6e068f08b214e5c2c
Kunal Jain <kunal.jain@peoplegrove.com>
2025-10-19 13:14:35 +0530

    feat: added helper method to abortForCancelledNode

---
+++ workers/mainWorker/helpers/abortForCancelledNode.js
@@ -0,0 +1,12 @@
+const { WORKFLOW_NODE_EXECUTION_STATUS } = require("../../../constants/workflowExecution");
+
+const abortForCancelledNode = async (nodeExecution) => {
+  await nodeExecution.reload();
+  if (nodeExecution.status === WORKFLOW_NODE_EXECUTION_STATUS.CANCELLED) {
+    const error = new Error("Node execution has been cancelled");
+    error.code = "WF-ABORT";
+    throw error;
+  }
+}
+
+module.exports = abortForCancelledNode;
